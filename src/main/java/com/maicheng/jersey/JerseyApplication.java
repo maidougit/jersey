78b5612d9343d31e6f7825409a6f47165da5d71c
maidou <maidousuiyue@163.com>
2018-07-03 01:25:30 +0800

    修改配置文件

--- src/main/java/com/maicheng/jersey/JerseyApplication.java
+++ src/main/java/com/maicheng/jersey/JerseyApplication.java
@@ -1,9 +1,11 @@
 package com.maicheng.jersey;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class JerseyApplication {
 
 	public static void main(String[] args) {
